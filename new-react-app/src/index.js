caa1978201709c793d3df8df58b96ea02f3e4a44
syukai <d_hirayama@rapid.ocn.ne.jp>
2019-06-15 16:05:13 +0900

    react MainConcept 4 Ceparate Components,  prepare

--- new-react-app/src/index.js
+++ new-react-app/src/index.js
@@ -70,8 +70,35 @@ ReactDOM.render(
     </div>,
     document.getElementById('welcomes')
 );
+function formatDate(dateValue) {
+    return dateValue.toLocaleDateString() + ' ' + dateValue.toLocaleTimeString();
+}
+function Comment(props) {
+    return (
+      <div className="Comment">
+        <div className="UserInfo">
+          <img className="Avatar"
+            src={props.author.avatarUrl}
+            alt={props.author.name}
+          />
+          <div className="UserInfo-name">
+            {props.author.name}
+          </div>
+        </div>
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    );
+  }
 
-
+ReactDOM.render(
+    <Comment date={new Date()}  text="planetext" author={{name: 'taro', avatarUrl: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'}} />,
+    document.getElementById('comment')
+);
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
