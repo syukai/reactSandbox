3d0104d86806601d50be74a8ebdaa7fa1dfe8cf4
syukai <d_hirayama@rapid.ocn.ne.jp>
2019-06-15 14:34:02 +0900

    react MainConcept 3

--- new-react-app/src/index.js
+++ new-react-app/src/index.js
@@ -4,11 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const name = "words in variable";
+const element = <h1>show "name" variable:{name}</h1>
 
 ReactDOM.render(
-    <h1>Hello World!</h1>,
+    element,
     document.getElementById('root')
 );
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Tick-Tock component</h1>
+            <h2>It is {new Date().toLocaleDateString()} {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('tick-tock')
+    );
+}
+
+setInterval(tick, 1000);
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
