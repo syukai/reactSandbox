29b4133235adc3b6444a86bfaefe3d27332a5f9f
syukai <d_hirayama@rapid.ocn.ne.jp>
2019-06-15 16:31:19 +0900

    cut out Avatar

--- new-react-app/src/index.js
+++ new-react-app/src/index.js
@@ -77,9 +77,7 @@ function Comment(props) {
     return (
       <div className="Comment">
         <div className="UserInfo">
-          <img className="Avatar"
-            src={props.author.avatarUrl}
-            alt={props.author.name}
+          <Avatar user={props.author}
           />
           <div className="UserInfo-name">
             {props.author.name}
@@ -94,6 +92,16 @@ function Comment(props) {
       </div>
     );
   }
+
+// Avatarを切り出し
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+        ></img>
+    )
+}
 
 ReactDOM.render(
     <Comment date={new Date()}  text="planetext" author={{name: 'taro', avatarUrl: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'}} />,
